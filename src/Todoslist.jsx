1e13b8cd0e4c19da178519ca3e292d413f814173
sayra75 <sayrasoni75@gmail.com>
2024-07-10 15:36:54 +0530

    Update Todoslist.jsx

--- src/Todoslist.jsx
+++ src/Todoslist.jsx
@@ -18,15 +18,11 @@ function Todoslist(props) {
       id="text"
       key={props.index}
       value={props.item}
-      style={props.complete ? props.mystylef : {}}
-    >
-      <div className="svg" style={props.complete ? { display: "flex" } : {}}>
-        {" "}
+      style={props.complete ? props.mystylef : {}} >
+      <div className="svg" style={props.complete ? { display: "flex" } : {}}>       
         <svg xmlns="http://www.w3.org/2000/svg " viewBox="0 0 448 512">
-          <path
-            d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5
--32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z "
-          />
+          <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5
+-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z "/>
         </svg>
       </div>
       <button
@@ -37,7 +33,7 @@ function Todoslist(props) {
         style={props.complete ? props.mystylef : {}}
       >
         <div style={props.complete ? props.mystyle : {}} className="task">
-          {" "}
+         
           {props.initial}{" "}
         </div>
       </button>
